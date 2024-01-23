2c206586b1d67197a4c24475af2dcbf9b0a28e9e
Chadi Ejja <ch.ejja@hotmail.com>
2024-01-23 15:01:52 +0100

    hompage wip

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create!(email: "user@hotmail.com", password: "123456", phone_number: "0021264879856")
+User.create!(email: "coiffeur@hotmail.com", password: "123456", phone_number: "0021264879856")
+
+Coiffeur.create!(user_id: User.second.id, )
+
