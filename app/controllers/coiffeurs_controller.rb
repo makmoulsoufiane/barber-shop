7e0475ced71d1de7113df169907e4b6848df350e
makmoul soufiane <makmoulsoufiane2@gmail.com>
2024-01-25 11:44:00 +0100

    contorller tarik

--- app/controllers/coiffeurs_controller.rb
+++ app/controllers/coiffeurs_controller.rb
@@ -1,12 +1,11 @@
 class CoiffeursController < ApplicationController
   def index
     @coiffeurs = Coiffeur.all
+    @user = current_user
   end
-
   def new
     @coiffeur= Coiffeur.new
   end
-
   def show
     @coiffeur =Coiffeur.find(params[:id])
   end
