21c8ccafa9c2bac4c680efe8bc48db6ae9ae9a6c
makmoul soufiane <makmoulsoufiane2@gmail.com>
2024-01-26 15:30:27 +0100

    controller booking

--- app/controllers/coiffeurs_controller.rb
+++ app/controllers/coiffeurs_controller.rb
@@ -1,10 +1,13 @@
 class CoiffeursController < ApplicationController
+  skip_before_action :authenticate_user!,only:[ :index, :show]
   def index
     @coiffeurs = Coiffeur.all
   end
+
   def new
     @coiffeur= Coiffeur.new
   end
+
   def show
     @coiffeur =Coiffeur.find(params[:id])
     @booking = Booking.new
