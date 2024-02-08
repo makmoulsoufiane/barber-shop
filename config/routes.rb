ddafee2c049919cc81bdb74789def7f82aa2b817
Chadi Ejja <ch.ejja@hotmail.com>
2024-02-08 16:41:23 +0100

    about us

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :bookings
   post "/accept", to: "bookings#accept"
   post "/reject", to: "bookings#reject"
-
+  get '/about',  to: "pages#about", as: 'about'
 end
